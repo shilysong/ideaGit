63711a9300f1f53abbf4105eee478262cfec6338
king <2014851402@qq.com>
2020-09-26 17:21:44 +0800

    修改HelloGit.class

--- idea-git-test/src/com/itnoobking/HelloGit.java
+++ idea-git-test/src/com/itnoobking/HelloGit.java
@@ -11,11 +11,12 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        //HelloGit
     }
 
     public void test() {
         int i=0;
         i+=10;
-
+        System.out.println(i);
     }
 }
