d972ae9a6fba7c739e6d8044ec2c2f1a670c6a88
king <2014851402@qq.com>
2020-09-26 17:11:11 +0800

    添加idea-git-test文件

---
+++ idea-git-test/src/com/itnoobking/HelloGit.java
@@ -0,0 +1,21 @@
+package com.itnoobking;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+
+    public void test() {
+        int i=0;
+        i+=10;
+
+    }
+}
